ead54df15d4661fe8d43eb4f313a65f87caff016
code1218 <skjil1218@gmail.com>
2022-11-15 12:21:46 +0900

    챕터1 출력

---
+++ src/main/java/j01_출력/UserInfo.java
@@ -0,0 +1,15 @@
+package j01_출력;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "임지현";
+        String address = "부산 동래구 사직동";
+        String phone = "010-9988-1916";
+        int age = -29;
+
+        System.out.println(name + "의 주소는 " + address + "입니다.");
+        System.out.println(name + "의 연락처는 " + phone + "입니다.");
+        System.out.println(name + "의 나이는 " + age + "살입니다.");
+
+    }
+}
