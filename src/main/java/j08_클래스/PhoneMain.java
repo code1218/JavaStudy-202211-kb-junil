667f655f5c4e5d92ce0ba1fb03985b6ef493dc78
code1218 <skjil1218@gmail.com>
2022-11-17 12:54:41 +0900

    클래스, 접근지정자

---
+++ src/main/java/j08_클래스/PhoneMain.java
@@ -0,0 +1,55 @@
+package j08_클래스;
+
+public class PhoneMain {
+
+    public static void main(String[] args) {
+
+        Phone iPhone14 = new Phone();
+        Phone galaxyS22 = new Phone("SAMSUNG", "GalaxyS22");
+        System.out.println(iPhone14);
+        System.out.println(galaxyS22);
+
+        iPhone14.company = "Apple";
+
+        iPhone14.printPhoneCompany();
+        iPhone14.printPhoneModel();
+
+        galaxyS22.printPhoneCompany();
+        galaxyS22.printPhoneModel();
+
+
+    }
+
+}
+
+/*
+
+    Class: Student, StudentMain
+    학생 정보
+    String school(학교명)
+    String name(학생이름)
+    int studentYear(학년)
+    boolean gender(성별 true=남, false=여)
+
+    showStudentInfo() {
+        학교명: 부산대학교
+        이름: 홍길동
+        학년: 3
+        성별: 남
+    }
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
