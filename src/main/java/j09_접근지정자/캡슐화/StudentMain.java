7d63311fb688c43ec3979e8af1b1c38362c2e92d
code1218 <skjil1218@gmail.com>
2022-11-18 12:55:32 +0900

    getter, setter, Array

--- src/main/java/j09_접근지정자/캡슐화/StudentMain.java
+++ src/main/java/j09_접근지정자/캡슐화/StudentMain.java
@@ -4,8 +4,15 @@ public class StudentMain {
 
     public static void main(String[] args) {
         Student s1 = new Student("김준일", "남");
-
         s1.printInfo();
+
+        s1.setName("김준이");
+        s1.printInfo();
+
+        s1.gender = "여";
+        s1.printInfo();
+        System.out.println(s1.gender);
+        System.out.println(s1.getName());
 
     }
 }
