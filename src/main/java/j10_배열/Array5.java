618ee5e43c277ab3b94eb04c2c4b67d3b794c2a2
code1218 <skjil1218@gmail.com>
2022-11-21 12:55:08 +0900

    상속, 업, 다운 캐스팅

--- src/main/java/j10_배열/Array5.java
+++ src/main/java/j10_배열/Array5.java
@@ -1,6 +1,11 @@
 package j10_배열;
 
 public class Array5 {
+
+    public static int getMinNumber(int num1, int num2) {
+        return num1 < num2 ? num1 : num2;
+    }
+
     public static void main(String[] args) {
         int[] nums1 = new int[] {1, 8, 9, 16, 17};
         int[] nums2 = new int[] {2, 7, 10, 15, 18};
@@ -12,23 +17,13 @@ public class Array5 {
         배열들 중 동일 인덱스에 위치한 값들 중 최소값들만 뽑아서 새로운 배열을 만든다.
          */
 
-        int a = 10;
-        int b = 5;
-        int c = 7;
-
-        int min = 0;
-
-        if(a < b) {
-            min = a;
-        }else {
-            min = b;
+        for(int i = 0; i < nums1.length; i++) {
+            resultArray[i]
+                    = getMinNumber(getMinNumber(nums1[i], nums2[i]), getMinNumber(nums3[i], nums4[i]));
+            System.out.println(resultArray[i]);
         }
 
-        if(c < min) {
-            min = c;
-        }
 
-        System.out.println("min: " + min);
 
 
     }
---
+++ src/main/java/j11_상속/C.java
@@ -0,0 +1,11 @@
+package j11_상속;
+
+public class C {
+
+    public String a;
+
+    public String getA() {
+        return a;
+    }
+
+}
